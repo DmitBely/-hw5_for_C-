6dca7ff59f1dc37019c765c8c07044cf8a8abd62
dmitriy <dmitbely@protonmail.ch>
2022-09-07 19:28:52 +0300

    tak001

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,6 +1,20 @@
 ﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, 
 // y = k2 * x + b2, b1 k1 и b2 и k2 заданы
 
+Console.WriteLine("Введите значение b1:");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k1:");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2:");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2:");
+int k2 = int.Parse(Console.ReadLine());
 
 
 
+int x = (b2 - b1)/(k1 - k2);
+int y = k2 * x + b2;
+
+
+
+Console.WriteLine($"x = {x}, y = {y}");
